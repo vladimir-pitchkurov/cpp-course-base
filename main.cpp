9dcdee9541fd50a34f90424e7ddb22a4de0502b3
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-14 22:41:37 +0200

    while pp6

--- main.cpp
+++ main.cpp
@@ -6,9 +6,16 @@ int main() {
 //По данному числу N распечатайте все целые степени двойки, не превосходящие N, в порядке возрастания.
     int n, counter = 1;
     cin >> n;
-    while (counter <= n) {
-        cout << counter << " ";
-        counter = counter * 2;
+    while (true) {
+        if (n == counter) {
+            cout << "YES";
+            break;
+        }
+        if (counter > n) {
+            cout << "NO";
+            break;
+        }
+        counter *= 2;
     }
     return 0;
 }
