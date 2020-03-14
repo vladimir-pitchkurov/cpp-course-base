5b513cfc7ebe68deb1fc2127b31c35ccf1beca5b
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-14 16:14:14 +0200

    https://stepik.org/lesson/13021/step/3?unit=4362

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    Белочки и орешки - 1
+//    N белочек нашли K орешков и решили разделить их поровну. Определите, сколько орешков достанется каждой белочке.
+
+    int n, k;
+    cin >> n >> k;
+    cout << (k / n) << endl;
     return 0;
 }
