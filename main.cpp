78db1be09ec2b8e2f52d5b203aa0483a106e76ef
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-14 21:59:42 +0200

    while 3

--- main.cpp
+++ main.cpp
@@ -3,37 +3,17 @@
 using namespace std;
 
 int main() {
-// Есть две коробки, первая размером A1×B1×C1, вторая размером A2×B2×C2.
-// Определите, можно ли разместить одну из этих коробок внутри другой,
-// при условии, что поворачивать коробки можно только на 90 градусов вокруг ребер.
-// Формат входных данных
-//Программа получает на вход числа A1, B1, C1, A2, B2, C2.
-//Формат выходных данных
-//Программа должна вывести одну из следующих строчек:
-//Boxes are equal, если коробки одинаковые,
-//The first box is smaller than the second one, если первая коробка может быть положена во вторую,
-//The first box is larger than the second one, если вторая коробка может быть положена в первую,
-//Boxes are incomparable, во всех остальных случаях.
-
-    int a, b, c, a1, b1, c1;
-    cin >> a >> b >> c >> a1 >> b1 >> c1;
-    if ((a == a1 && b == b1 && c == c1)
-        || (b == a1 && c == b1 && a == c1)
-        || (c == a1 && a == b1 && b == c1)
-        || (c == a1 && b == b1 && a == c1)) {
-        cout << "Boxes are equal";
-    } else if ((a >= a1 && b >= b1 && c >= c1)
-               || (b >= a1 && c >= b1 && a >= c1)
-               || (c >= a1 && a >= b1 && b >= c1)
-               || (c >= a1 && b >= b1 && a >= c1)) {
-        cout << "The first box is larger than the second one";
-    } else if ((a <= a1 && b <= b1 && c <= c1)
-               || (b <= a1 && c <= b1 && a <= c1)
-               || (c <= a1 && a <= b1 && b <= c1)
-               || (c <= a1 && b <= b1 && a <= c1)) {
-        cout << "The first box is smaller than the second one";
-    } else {
-        cout << "Boxes are incomparable";
+// По данному целому числу N распечатайте все квадраты натуральных чисел, не превосходящие N, в порядке возрастания.
+    int n, counter = 1, pow;
+    cin >> n;
+    while (true) {
+        pow = counter * counter;
+        if (pow <= n) {
+            cout << pow << " ";
+        } else {
+            break;
+        }
+        counter++;
     }
     return 0;
 }
