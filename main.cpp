d3b0c5e3b619d113f5ff2019353f6dd9c727e1d2
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-14 22:47:17 +0200

    while pp6 BEST WAY

--- main.cpp
+++ main.cpp
@@ -3,19 +3,8 @@
 using namespace std;
 
 int main() {
-//По данному числу N распечатайте все целые степени двойки, не превосходящие N, в порядке возрастания.
-    int n, counter = 1;
+    int n;
     cin >> n;
-    while (true) {
-        if (n == counter) {
-            cout << "YES";
-            break;
-        }
-        if (counter > n) {
-            cout << "NO";
-            break;
-        }
-        counter *= 2;
-    }
+    cout << (!(n & (n - 1)) ? "YES" : "NO");
     return 0;
 }
