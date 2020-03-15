ac5e94bbb79158ae2865174aff8f2e8f7ab80f8d
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-15 17:39:12 +0200

    https://stepik.org/lesson/13024/step/9?thread=solutions&unit=4376

--- main.cpp
+++ main.cpp
@@ -5,15 +5,26 @@
 using namespace std;
 
 int main() {
-    // Определите среднее значение всех элементов последовательности, завершающейся числом
+    // Дана последовательность натуральных чисел x1, x2, ..., xn. Стандартным отклонением называется величина
+    //
+    // https://ucarecdn.com/a5272a3c-f857-4969-a054-0a362300c32d/
+    //
+    // где
+    // https://ucarecdn.com/f6612807-4fb8-42fc-802e-5221860e8fa2/
+    //
+    //- среднее значение последовательности.
+    // Определите стандартное отклонение для данной последовательности натуральных чисел, завершающейся числом 0.
 
-    double x, sum = 0;
+    double x, sum = 0, s, pows = 0;
     int counter = 0;
     while (cin >> x && x) {
-        counter++;
+        pows = pows + x * x;
         sum = sum + x;
+        counter++;
     }
     cout << setprecision(11) << fixed;
-    cout << (sum / counter);
+    cout << sqrt(
+            (pows - (sum * sum / counter)) / (counter - 1)
+    );
     return 0;
 }
