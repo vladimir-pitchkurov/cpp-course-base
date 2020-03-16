f22efdba0757028593b2376f7393475a83000c31
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-16 21:52:10 +0200

    arrays, vectors 8 chess

--- main.cpp
+++ main.cpp
@@ -8,24 +8,28 @@ using namespace std;
 int main() {
     // Дан массив. Выведите те его элементы, которые встречаются в массиве только один раз.
     // Элементы нужно выводить в том порядке, в котором они встречаются в списке.
-    int n, count = 0;
-    cin >> n;
+    int n = 16;
     vector<int> a(n);
+    int yes = 0;
 
     for (int i = 0; i < n; i++)
         cin >> a[i];
 
-    for (int i = 0; i < n; i++) {
-        count = 0;
-        for (int j = 0; j < n; j++) {
-            if (i != j && a[i] == a[j]) {
-                count++;
+    for (int i = 1; i < n && yes == 0; i += 2) {
+        for (int j = 1; j < n; j += 2) {
+            if (
+                    i != j
+                    && (
+                            a[i] == a[j]
+                            || a[i - 1] == a[j - 1]
+                            || (a[i] - a[j]) * (a[i] - a[j]) == (a[i - 1] - a[j - 1]) * (a[i - 1] - a[j - 1])
+                    )
+                    ) {
+                yes = 1;
+                break;
             }
         }
-        if (count == 0) {
-            cout << a[i] << " ";
-        }
     }
-
+    cout << ((yes) ? "YES" : "NO");
     return 0;
 }
