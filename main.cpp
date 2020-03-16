6ff5a68f03ec1d22ce2b5a4ebe62328349c031c2
vladimir-pitchkurov <vladimir.pitchkurov@gmail.com>
2020-03-16 21:15:10 +0200

    arrays, vectors https://stepik.org/lesson/13025/step/12?thread=solutions&unit=4377 V_2 genius))

--- main.cpp
+++ main.cpp
@@ -9,21 +9,14 @@ int main() {
     // Циклически сдвиньте элементы списка вправо (A[0] переходит на место A[1], A[1] на место A[2], ...,
     // последний элемент переходит на место A[0]).
     int n;
-    int tmp;
     cin >> n;
     vector<int> a(n);
 
-    for (int i = 1; i <= n; i++) {
-        cin >> tmp;
-        if (i == n) {
-            a[0] = tmp;
-            break;
-        }
-        a[i] = tmp;
-    }
-    for (auto x:a) {
-        cout << x << " ";
-    }
+    for (int i = 1; i <= n; i++)
+        cin >> a[i%n];
+
+    for (int i : a)
+        cout << i << " ";
 
     return 0;
 }
